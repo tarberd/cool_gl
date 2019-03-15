3208c7966fdc73e434b600662a3b5f2a0587f84a
tarberd <bernardo.mferrari@gmail.com>
2019-03-14 21:47:55 -0300

    add simple window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <gtkmm.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "hellow" << std::endl;
-    return 0;
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+    Gtk::Window window;
+
+    return app->run(window);
 }
