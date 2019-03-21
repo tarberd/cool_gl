99a98ad877d1d4e1dffc2d026539e30a8dc34b89
tarberd <bernardo.mferrari@gmail.com>
2019-03-21 09:26:49 -0300

    Create geometry objects.

--- cool/main.cpp
+++ cool/main.cpp
@@ -101,6 +101,19 @@ int main(int argc, char **argv) {
                 cool_gl::Vec3{0.0, 40.0, 1.0},
                 cool_gl::Colour{0.8, 0.0, 0.0}});
 
+    drawable_vector.emplace_back(
+            new cool_gl::Point{
+                cool_gl::Vec3{12.0,20.0,1.0},
+                cool_gl::Colour{0.2, 0.7, 0.4}});
+
+    using cool_gl::Vec3;
+
+    drawable_vector.emplace_back(
+            new cool_gl::Polygon{
+                std::vector<Vec3>{Vec3{2.0, 2.0, 1.0}, Vec3{2.0, 38.0, 1.0}, Vec3{38.0, 38.0, 1.0}, Vec3{38.0, 2.0, 1.0}},
+                cool_gl::Colour{0.7,0.2,0.4}});
+
+
   auto app = Gtk::Application::create(argc, argv, "Cool.gl");
 
   auto builder = Gtk::Builder::create();
--- libcool_gl/include/cool_gl/cool_gl.h
+++ libcool_gl/include/cool_gl/cool_gl.h
@@ -10,10 +10,10 @@ struct Vec3 {
   double z;
 };
 
-enum class Drawable_type : int { point, line };
+enum class Drawable_type : int { point, line, polygon};
 
 struct Drawable {
-public:
+  virtual ~Drawable();
   virtual void draw(const Cairo::RefPtr<Cairo::Context> &cr, Vec3 window_min, Vec3 window_max, Vec3 viewport_min, Vec3 viewport_max) const = 0;
   virtual Drawable_type type() const noexcept = 0;
 };
@@ -22,6 +22,19 @@ struct Colour {
   double r;
   double g;
   double b;
+};
+
+struct Point : public Drawable {
+  Vec3 position;
+  Colour colour;
+
+  template <class A, class B>
+  Point(A &&position, B &&colour) noexcept
+      : position{std::forward<A>(position)}, colour{std::forward<B>(colour)} {}
+
+  void draw(const Cairo::RefPtr<Cairo::Context> &cr, Vec3 window_min, Vec3 window_max, Vec3 viewport_min, Vec3 viewport_max) const final;
+
+  Drawable_type type() const noexcept final;
 };
 
 struct Line : public Drawable {
@@ -35,9 +48,24 @@ struct Line : public Drawable {
   Line(Line &&) = default;
 
   template <class A, class B, class C>
-  Line(A &&begin, B &&end, C &&colour)
+  Line(A &&begin, B &&end, C &&colour) noexcept
       : begin{std::forward<A>(begin)}, end{std::forward<B>(end)},
         colour{std::forward<C>(colour)} {}
+
+  void draw(const Cairo::RefPtr<Cairo::Context> &cr, Vec3 window_min, Vec3 window_max, Vec3 viewport_min, Vec3 viewport_max) const final;
+
+  Drawable_type type() const noexcept final;
+};
+
+struct Polygon : public Drawable {
+  std::vector<Vec3> points;
+
+  Colour colour;
+
+  template <class A, class B>
+  Polygon(A &&points, B &&colour) noexcept
+      : points{std::forward<A>(points)},
+        colour{std::forward<B>(colour)} {}
 
   void draw(const Cairo::RefPtr<Cairo::Context> &cr, Vec3 window_min, Vec3 window_max, Vec3 viewport_min, Vec3 viewport_max) const final;
 
