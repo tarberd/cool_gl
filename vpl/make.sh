9a93045e7ff4feb1fbe034b48186bb6723455184
matheusteixeira <matheusteixeira01@live.com>
2019-04-25 22:53:24 -0300

    Update to_vpl script

--- vpl/make.sh
+++ vpl/make.sh
@@ -11,4 +11,4 @@ echo -e "Compilando e linkando..."
 #
 echo "Usando: gcc `pkg-config --cflags gtk+-3.0 gtkmm-3.0`-o vpl_wexecution $SOURCE_FILES `pkg-config --libs gtk+-3.0 gtkmm-3.0` -rdynamic -lstdc++ -std=c++11 -lm"
 echo "ERROS E AVISOS (se houver):"
-gcc `pkg-config --cflags gtk+-3.0 gtkmm-3.0`-o vpl_wexecution $SOURCE_FILES `pkg-config --libs gtk+-3.0 gtkmm-3.0` -rdynamic -lstdc++ -std=c++11 -lm
+gcc `pkg-config --cflags gtk+-3.0 gtkmm-3.0` -o vpl_wexecution $SOURCE_FILES `pkg-config --libs gtk+-3.0 gtkmm-3.0` -rdynamic -lstdc++ -std=c++11 -lm
--- vpl/to_vpl.sh
+++ vpl/to_vpl.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 cp ../cool/*.{,cpp,h} .
+cp ../cool/src/*{,cpp,h} .
 cp ../cool/data/*.glade .
 cp ../cool_gl/src/*.cpp .
+cp ../cool_gl/src/*.h .
 cp ../cool_gl/include/cool_gl/*.h .
 
 sed -i 's/#include <cool_gl\/\(\w*\).h>/#include "\1.h"/g' *
