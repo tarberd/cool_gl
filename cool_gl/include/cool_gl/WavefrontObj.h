432bea8b9a8b68cf84ccfbd769d7d234f3dafb17
tarberd <bernardo.mferrari@gmail.com>
2019-05-03 13:40:13 -0300

    First Wavefront obj draft

--- cool_gl/include/cool_gl/WavefrontObj.h
+++ cool_gl/include/cool_gl/WavefrontObj.h
@@ -1,9 +1,33 @@
 #pragma once
 
+#include "Drawable.h"
 #include "Polygon.h"
 #include <istream>
 
 namespace cool_gl {
 
-Polygon make_polygon_from_obj(const std::istream &input) noexcept;
+class WavefrontObj : public Drawable {
+public:
+  Drawable *copy() const noexcept;
+
+  void draw(const Cairo::RefPtr<Cairo::Context> &cr, Vec viewport_min,
+            Vec viewport_max) const final;
+
+  void transform(const Matrix &transform) noexcept;
+
+  Vec mass_centre() noexcept;
+
+  std::string type() const noexcept;
+
+  const std::string &name() const noexcept;
+  std::string &name() noexcept;
+
+  std::vector<Vec> vertex_data;
+
+  std::vector<std::tuple<int, int, int>> faces;
+
+  std::string m_name;
+};
+
+WavefrontObj make_polygon_from_obj(const std::istream &input) noexcept;
 }
--- cool_gl/src/WavefrontObj.cpp
+++ cool_gl/src/WavefrontObj.cpp
@@ -1,9 +1,31 @@
 #include <cool_gl/WavefrontObj.h>
 
 namespace cool_gl {
+Drawable *WavefrontObj::copy() const noexcept {
+  return new WavefrontObj{*this};
+}
 
-Polygon make_polygon_from_obj(const std::istream &input) noexcept {
-  return Polygon{std::vector<Vec>{}, Colour{0.0, 0.0, 0.0}, ""};
+void WavefrontObj::draw(const Cairo::RefPtr<Cairo::Context> &cr,
+                        Vec viewport_min, Vec viewport_max) const {}
+
+void WavefrontObj::transform(const Matrix &transform) noexcept {}
+
+Vec WavefrontObj::mass_centre() noexcept {
+  return Vec{0.0, 0.0, 0.0};
+}
+
+std::string WavefrontObj::type() const noexcept { return "WavefrontObj"; }
+
+const std::string &WavefrontObj::name() const noexcept { return m_name; }
+
+std::string &WavefrontObj::name() noexcept { return m_name; }
+
+WavefrontObj make_polygon_from_obj(const std::istream &input) noexcept {
+  std::vector<Vec> vertex_data;
+  std::vector<std::tuple<int, int, int>> faces;
+  std::string name;
+
+  return WavefrontObj{};
 }
 
 } // namespace cool_gl
